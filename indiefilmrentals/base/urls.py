3a0d253b5466076848800305b6edaf5974a1f5e5
Nat Hagey <nat.hagey@gmail.com>
2012-05-22 20:40:37 -0400

    category

--- indiefilmrentals/base/urls.py
+++ indiefilmrentals/base/urls.py
@@ -5,5 +5,7 @@ from django.conf.urls.defaults import url, patterns
 
 urlpatterns = patterns('indiefilmrentals.base.views',
     url(r'^$', 'home', name='home'),
-    url(r'(?P<slug>.*?)$', 'product', name='product_view'),
+    url(r'^(?P<category>.*?)$', 'category', name='category_view'),
+    url(r'^(?P<category>.*?)/(?P<brand>.*?)$', 'brand', name='brand_view'),
+    url(r'^(?P<category>.*?)/(?P<brand>.*?)/(?P<slug>.*?)$', 'product', name='product_view'),
 )

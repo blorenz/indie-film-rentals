96b23886e73e93229181d6d15984902a86b990e0
Brandon <blorenz@gmail.com>
2012-08-14 19:21:26 -0400

    Latest SQL

--- indiefilmrentals/base/models.py
+++ indiefilmrentals/base/models.py
@@ -5,5 +5,5 @@ class Link(models.Model):
     anchor = models.CharField(max_length=200)
     url = models.URLField()
 
-    def _unicode__(self):
+    def __unicode__(self):
         return "%s | %s" % (self.anchor, self.url, )
